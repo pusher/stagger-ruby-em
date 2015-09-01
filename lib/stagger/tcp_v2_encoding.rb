474997b55ea7c0c33c617ec6dbb564fb3221b835
zimbatm <zimbatm@zimbatm.com>
2015-09-01 11:43:46 +0100

    Simplified the encoder design
    Things where too inter-mixed before

--- lib/stagger/tcp_v2_encoding.rb
+++ lib/stagger/tcp_v2_encoding.rb
@@ -3,42 +3,19 @@ require 'msgpack'
 
 module Stagger
   class EncodingError < StandardError; end
-  module TCPv2Encoding
-    extend self
-
+  class TCPv2EncodingImpl
     MAGIC_BYTE1 = "\x83"
     MAGIC_BYTE2 = "\x84"
     MAGIC_VERSION = "\x00"
     MAGIC_HEADER = [MAGIC_BYTE1, MAGIC_BYTE2, MAGIC_VERSION].join
 
-    @@method_to_byte = {}
-    @@byte_to_method = {}
-
-    private
-
-    def to_byte(method)
-      @@method_to_byte[method] ||
-        fail(ArgumentError, "Unknown method #{method}")
+    def initialize(method_to_byte)
+      @method_to_byte = method_to_byte.freeze
+      @byte_to_method = method_to_byte.each_with_object({}) do |(method, byte), hash|
+        hash[byte] = method
+      end.freeze
+      freeze
     end
-
-    def to_method(byte)
-      @@byte_to_method[byte] ||
-        fail(ArgumentError, "Unknown byte #{byte.inspect} for method")
-    end
-
-    def def_method(method, byte)
-      @@method_to_byte[method] = byte
-      @@byte_to_method[byte] = method
-    end
-
-    def_method(:'pair:ping', "\x28")
-    def_method(:'pair:pong', "\x29")
-    def_method(:'report_all', "\x30")
-    def_method(:'register_process', "\x41")
-    def_method(:'stats_partial', "\x42")
-    def_method(:'stats_complete', "\x43")
-
-    public
 
     def decode(buffer) #=> new_buffer, method, params
       return nil, nil, nil if buffer.nil?
@@ -62,5 +39,26 @@ module Stagger
       method = to_byte(method)
       MAGIC_HEADER + method + [params.bytesize].pack('N') + params
     end
+
+    private
+
+    def to_byte(method)
+      @method_to_byte[method] ||
+        fail(ArgumentError, "Unknown method #{method}")
+    end
+
+    def to_method(byte)
+      @byte_to_method[byte] ||
+        fail(ArgumentError, "Unknown byte #{byte.inspect} for method")
+    end
   end
+
+  TCPv2Encoding = TCPv2EncodingImpl.new(
+    :'pair:ping' => "\x28",
+    :'pair:pong' => "\x29",
+    :'report_all' => "\x30",
+    :'register_process' => "\x41",
+    :'stats_partial' => "\x42",
+    :'stats_complete' => "\x43",
+  )
 end
