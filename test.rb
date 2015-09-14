94401798a2a82a2ba2067c832771e999c4303bf9
Michael Pye <mail@mdpye.co.uk>
2015-09-14 14:37:43 +0100

    Case for replication

--- test.rb
+++ test.rb
@@ -2,18 +2,25 @@
 # -*- coding: utf-8 -*-
 require 'bundler/setup'
 require 'stagger'
+
 EM.error_handler do |err|
   p [:error, err, *(err.backtrace rescue nil)]
 end
+
 EM.run {
   (1..20).each do |i|
     Stagger.default.register_value(:"app.connections", account_id: i%3, app_id: i) {
-      puts "fetching data for app.connections#{i}"
       Random.rand
     }
   end
-
+  pause = true
   Stagger.default.register_cb { |agg|
+    if pause
+      puts "Long tick"
+      sleep 21
+      puts "done"
+      pause = false
+    end
     agg.incr(:"app.woot", 1, account_id: rand(100))
   }
 
